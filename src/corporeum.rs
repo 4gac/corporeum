49bb9c9e5e19ad180453460f76a7727979bef8dc
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-05-12 16:12:03 +0200

    feat: add support for optional compression

--- src/corporeum.rs
+++ src/corporeum.rs
@@ -1,5 +1,9 @@
 use crate::schema::Corpus;
-use std::{fs, io::Write, path::Path};
+use std::{
+    fs,
+    io::{BufReader, Cursor, Read, Result as IoResult, Write},
+    path::Path,
+};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Compression {
@@ -24,71 +28,70 @@ impl Corporeum<'_> {
     }
 
     // function to load an already existing corpus
-    pub fn load<P: AsRef<Path>>(source: &P) -> Corporeum {
-        let data = if source.as_ref().is_file() {
-            // FIXME error handling
-            fs::read(source).expect("Unable to read file")
-        } else {
-            panic!("Not a file");
-        };
+    pub fn load<P: AsRef<Path>>(source: &P) -> IoResult<Corporeum> {
+        let path = source.as_ref();
+        let file = fs::OpenOptions::new().read(true).open(path)?;
+        let compression = path.try_into().expect("Unknown format");
+        let mut data = Vec::new();
 
-        // parse json file
+        match compression {
+            Compression::None => (),
+            Compression::Deflate => {
+                let mut decompressor = flate2::read::GzDecoder::new(file);
+                decompressor.read_to_end(&mut data)?;
+            }
+            Compression::Lzma => {
+                let mut reader = BufReader::new(file);
+                lzma_rs::xz_decompress(&mut reader, &mut data).unwrap();
+            }
+        }
+
         let corpus: Corpus = serde_cbor::from_slice(&data).unwrap();
-        // let mut corpus: Corpus = serde_json::from_str(&data).unwrap();
-        // let mut corp: Corpus =
-        // serde_pickle::from_slice(&data.as_bytes(), Default::default()).unwrap();
-        // iterate over docs and setup last sentence id,
-        // so we do not have search for last available id every time we add new sentence
-        // corpus
-        //     .documents
-        //     .iter_mut()
-        //     .for_each(|doc| doc.last_sentence_id = doc.setup_last_sentence_id());
-
-        Corporeum {
+        Ok(Corporeum {
             original_file_path: source.as_ref(),
             corpus,
-        }
+        })
     }
 
-    pub fn save_cbor(&self, packed: bool) {
+    pub fn save_cbor(&self, packed: bool) -> IoResult<()> {
+        self.save_as_cbor(
+            &self.original_file_path,
+            packed,
+            self.original_file_path.try_into().unwrap(),
+        )
+    }
+
+    pub fn save_as_cbor<P: AsRef<Path>>(
+        &self,
+        destination: &P,
+        packed: bool,
+        compression: Compression,
+    ) -> IoResult<()> {
         let buffer = if packed {
             serde_cbor::ser::to_vec_packed(&self.corpus).unwrap()
         } else {
             serde_cbor::ser::to_vec(&self.corpus).unwrap()
         };
 
-        let dest = Path::with_extension(self.original_file_path, "cbor");
-        let dest = dest.as_path();
-
-        let file = fs::OpenOptions::new()
+        let destination = destination.as_ref().with_extension("cbor");
+        let mut file = fs::OpenOptions::new()
             .write(true)
             .truncate(true)
             .create(true)
-            .open(dest);
+            .open(destination)?;
 
-        file.unwrap().write_all(&buffer).unwrap();
-    }
-
-    pub fn save_as_cbor<P: AsRef<Path>>(&self, destination: &P, packed: bool) {
-        let buffer = if packed {
-            serde_cbor::ser::to_vec_packed(&self.corpus).unwrap()
-        } else {
-            serde_cbor::ser::to_vec(&self.corpus).unwrap()
-        };
-
-        let destination = destination.as_ref();
-
-        if destination.is_file() {
-            let dest = Path::with_extension(destination, "cbor");
-            let dest = dest.as_path();
-
-            let file = fs::OpenOptions::new()
-                .write(true)
-                .truncate(true)
-                .create(true)
-                .open(dest);
-
-            file.unwrap().write_all(&buffer).unwrap();
+        match compression {
+            Compression::None => file.write_all(&buffer),
+            Compression::Deflate => {
+                let mut compressor =
+                    flate2::write::GzEncoder::new(file, flate2::Compression::best());
+                compressor.write_all(&buffer)
+            }
+            Compression::Lzma => {
+                let mut cursor = Cursor::new(buffer);
+                lzma_rs::xz_compress(&mut cursor, &mut file).unwrap();
+                Ok(())
+            }
         }
     }
 
@@ -98,5 +101,24 @@ impl Corporeum<'_> {
 
     pub fn corpus_mut(&mut self) -> &mut Corpus {
         &mut self.corpus
+    }
+}
+
+impl TryFrom<&Path> for Compression {
+    type Error = ();
+
+    fn try_from(value: &Path) -> Result<Self, Self::Error> {
+        match value
+            .extension()
+            .unwrap()
+            .to_ascii_lowercase()
+            .to_str()
+            .unwrap()
+        {
+            "gz" | "zip" => Ok(Self::Deflate),
+            "lz" | "xz" | "lzma" => Ok(Self::Lzma),
+            "corp" => Ok(Self::None),
+            _ => Err(()),
+        }
     }
 }
