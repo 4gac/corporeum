350c32924cd6d703e89f9ad30fbd17e5418b5288
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-11-04 10:40:37 +0100

    fix: remove serde flags

--- src/schema.rs
+++ src/schema.rs
@@ -4,11 +4,8 @@ use std::marker::PhantomData;
 
 /// Represents a text corpus.
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq, Eq)]
-#[serde(rename = "corpus")]
 pub struct Corpus {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) metadata: Option<Metadata>,
-    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub(crate) documents: Vec<Document>,
 }
 
@@ -18,17 +15,12 @@ pub struct Corpus {
 /// metadata to a [`Corpus`](Corpus), use [`add_metadata()`](Corpus::add_metadata) instead.
 #[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 pub struct Metadata {
-    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub(crate) authors: Vec<Author>,
     // this does not follow semantic versioning, it is just a number
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) created: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) description: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) modified: Option<i64>,
     pub(crate) name: String,
-    #[serde(rename = "version")]
     pub(crate) version: u16,
 }
 
@@ -40,7 +32,6 @@ pub struct Metadata {
 pub struct Author {
     pub(crate) first_name: String,
     pub(crate) last_name: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) mail: Option<String>,
 }
 /// Represents a document.
@@ -49,12 +40,9 @@ pub struct Author {
 /// to a [`Corpus`](Corpus), use [`add_doc()`](Corpus::add_doc) instead.
 #[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 pub struct Document {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) description: Option<String>,
     pub(crate) id: u32,
-    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub(crate) sentences: Vec<Sentence<Source>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) source: Option<String>,
 }
 
@@ -76,7 +64,6 @@ pub struct Sentence<Type> {
     pub(crate) lang: String, // TODO features, labels
     // pub(crate) sentence_type: SentenceType, // language identifier
     pub(crate) tokens: Vec<Token>,
-    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub(crate) translations: Vec<Sentence<Target>>,
 }
 
@@ -89,20 +76,12 @@ pub struct Sentence<Type> {
 pub struct Token {
     pub(crate) id: u32,
     pub(crate) form: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) lemma: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) upos: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) xpos: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) feats: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) head: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) deprel: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) deps: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) misc: Option<String>, // this is here only for compatibility reasons with CoNLL-U
 }
